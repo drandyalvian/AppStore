17e61418db5a40440dfbd44791c90a5fe7ca78bb
vendisaputra <vendisaputra@gmail.com>
2019-12-03 16:50:29 +0700

    bagian edit kepala cabang belum selesai di bagian drop downnya

--- app/src/main/java/com/example/company/appstore/Owner/EditTokoAct.java
+++ app/src/main/java/com/example/company/appstore/Owner/EditTokoAct.java
@@ -5,9 +5,13 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.company.appstore.R;
 import com.google.firebase.database.DataSnapshot;
@@ -16,13 +20,20 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+
 public class EditTokoAct extends AppCompatActivity {
 
     Button back,btnsave;
     EditText xnama, xpass;
     TextView  xusername, texttoko;
+    Spinner selectKepala;
 
-    DatabaseReference reference;
+    DatabaseReference reference, getSpinner;
+
+    ValueEventListener listener;
+    ArrayAdapter<String> arrayAdapter;
+    ArrayList<String> arrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,10 +46,32 @@ public class EditTokoAct extends AppCompatActivity {
         xpass = findViewById(R.id.xpass);
         texttoko = findViewById(R.id.texttoko);
         btnsave = findViewById(R.id.btnsave);
+        selectKepala = findViewById(R.id.selectKepala);
 
-//mengambil data dari intent
         Bundle bundle = getIntent().getExtras();
         final String cabangx= bundle.getString("nama");
+
+        getSpinner = FirebaseDatabase.getInstance().getReference().child("Cabang").child(cabangx).child("Karyawan");
+
+        arrayList = new ArrayList<>();
+        arrayAdapter = new ArrayAdapter<String>(EditTokoAct.this, android.R.layout.simple_spinner_dropdown_item, arrayList);
+
+        selectKepala.setAdapter(arrayAdapter);
+        getDataSpinner();
+
+        selectKepala.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(EditTokoAct.this, selectKepala.getSelectedItem().toString(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+//mengambil data dari intent
+
 
         reference = FirebaseDatabase.getInstance().getReference().child("KepalaCabang").child(cabangx);
         reference.addValueEventListener(new ValueEventListener() {
@@ -46,7 +79,7 @@ public class EditTokoAct extends AppCompatActivity {
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 xpass.setText(dataSnapshot.child("password").getValue().toString());
                 xusername.setText(dataSnapshot.child("username").getValue().toString());
-                xnama.setText(dataSnapshot.child("nama_lengkap").getValue().toString());
+                //xnama.setText(dataSnapshot.child("nama_lengkap").getValue().toString());
                 texttoko.setText(dataSnapshot.child("nama_cabang").getValue().toString());
             }
 
@@ -87,6 +120,34 @@ public class EditTokoAct extends AppCompatActivity {
             public void onClick(View v) {
                 Intent go = new Intent(EditTokoAct.this,OwnerDashbordAct.class);
                 startActivity(go);
+
+            }
+        });
+    }
+
+    private void getDataSpinner(){
+        listener = getSpinner.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot item:dataSnapshot.getChildren()){
+
+                    getSpinner.child(item.getValue().toString()).addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            arrayList.add(dataSnapshot.getRef().toString());
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                        }
+                    });
+                }
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
 
             }
         });
